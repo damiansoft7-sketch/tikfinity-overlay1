f02f0d46ede9efbcdc2590fe1f536f442594a812
damiansoft7-sketch <damian.soft7@gmail.com>
2026-07-16 14:41:12 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,52 @@
+// ===============================
+// FIREBASE
+// ===============================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    get,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDqdpAchewxlYmGrhFb5WyJKqbL5UP2XsA",
+
+    authDomain: "tikfinity-overlay.firebaseapp.com",
+
+    databaseURL: "https://tikfinity-overlay-default-rtdb.firebaseio.com",
+
+    projectId: "tikfinity-overlay",
+
+    storageBucket: "tikfinity-overlay.firebasestorage.app",
+
+    messagingSenderId: "943639302610",
+
+    appId: "1:943639302610:web:5e747094f2f6151b70380b"
+
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Base de datos
+const db = getDatabase(app);
+
+// Exportar funciones
+export {
+
+    db,
+
+    ref,
+
+    get,
+
+    set,
+
+    onValue
+
+};
